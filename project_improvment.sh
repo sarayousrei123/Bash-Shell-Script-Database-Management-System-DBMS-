d277bf673d2af10fd9f2f8855981ce39805bd7be
Sara Yousrei Alsoyefeai <129558718+sarayousrei123@users.noreply.github.com>
2025-03-23 07:21:01 +0300

    Add files via upload
    first try

---
+++ project_improvment.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+echo "***********************************************************************************************************"
+echo ""
+echo "                    Bash Shell Script Database Management System (DBMS)               "
+echo "                        Telecom Application Development - Intake 45                  "
+echo "                             Sara Yousrei Alsoyefeai Allsebeai                        "
+echo ""
+echo "***********************************************************************************************************"
+echo ""
+
+echo "Enter Your Option : "
+select choice in "Enter To Your Database" "Exit" ; do
+
+case $REPLY in
+	1) 
+	echo "Entering To Database ...."
+	;;
+	2)
+	echo "Exiting , Goodbye ..... "
+	exit 0
+	;;
+	*)
+	echo "Invalid OPtion PLease Choose Either FRom 1 or 2 ONly "
+	;;
+esac
+done 
