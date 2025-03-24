84c7193bf5a478f382ea1c1fd7afdd6d42f1b30d
Sara Yousrei Alsoyefeai <129558718+sarayousrei123@users.noreply.github.com>
2025-03-24 06:08:57 +0300

    added dropDB and renameDB

--- project_improvment.sh
+++ project_improvment.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(dirname "$0")"
 DB_MAIN_DIR="$SCRIPT_DIR/Databases"  
 
 if ! [[ -d "$DB_MAIN_DIR" ]]; then
-    mkdir -p "$DB_MAIN_DIR"  
+    mkdir -p "$DB_MAIN_DIR"  #DB_MAIN_DIR 
 fi
 
 echo "***********************************************************************************************************"
@@ -20,9 +20,11 @@ function welcomeScreen {
     PS3="Enter Your Option: "
     select choice in "Enter to your database" "Exit"; do
         case $REPLY in
-            1) dbMainMenu ;;  
-            2) echo "Exiting, Goodbye ..... "; exit 0 ;;  
-            *) echo "Invalid choice! Please enter 1 or 2." ;;  
+            1) dbMainMenu ;; 
+            2) echo "Exiting, Goodbye ..... "
+            	 exit 0 ;;   
+            *) echo "Invalid choice! Please enter 1 or 2" ;;
+         
         esac
     done
 }
@@ -33,27 +35,75 @@ function dbMainMenu {
         echo "********************** Main Menu ********************** "
 
         PS3="Enter your choice: "  
-        options=("Select DB" "Create DB" "Rename DB" "Drop DB" "Show DBs" "Execute SQL Query" "Exit" #"Back" )
+        options=("Select DB" "Create DB" "Rename DB" "Drop DB" "Show DBs" "Execute SQL Query" "Exit" )
 
         select choice in "${options[@]}"; do
             case $REPLY in
-                1) selectDB; break ;;  
-                2) createDB; break ;;  
-                3) renameDB; break ;;  
-                4) dropDB; break ;;  
-                5) showDBs; break ;;  
-                6) executeSQL; break ;;  
-          #      7) echo "Returning to Welcome Screen...";
-            #      	sleep 1; 
-             #     	clear; 
-           #       	return 0 
-             #    	;; 
+                1) selectDB ;; 
+                2) createDB ;;  
+                3) renameDB ;;
+                4) dropDB   ;;  
+                5) showDBs  ;;   
+                6) executeSQL ;;   
                 7) echo "Exiting..."; exit 0 ;;  
-                *) echo "Invalid option! Please enter a number between 1-8." ;;
+                *) echo "Invalid option! Please enter a number between 1-7" ;;
             esac
         done
     done
 }
 
+function renameDB {
+    echo -n "Enter the current database name: "
+    read old_name
+
+    old_path="$DB_MAIN_DIR/$old_name"
+
+    if [[ ! -d "$old_path" ]]; then
+        echo -e "\e[31mDatabase '$old_name' does not exist!\e[0m"
+        return
+    fi
+
+    echo -n "Enter the new database name: "
+    read new_name
+
+    new_path="$DB_MAIN_DIR/$new_name"
+
+    if [[ -d "$new_path" ]]; then
+        echo -e "\e[31mDatabase '$new_name' already exists!\e[0m"
+        return
+    fi
+
+    mv "$old_path" "$new_path"
+    echo -e "\e[32mDatabase renamed successfully from '$old_name' to '$new_name'!\e[0m"
+} 
+
+function dropDB {
+    echo -n "Enter the Database Name to delete: "
+    read DB_NAME
+
+    DB_PATH="$DB_MAIN_DIR/$DB_NAME"
+
+    if [[ -d "$DB_PATH" ]]; then
+        echo -n "Are you sure you want to delete this database '$DB_NAME'? [y/n]: "
+        read confirmation
+        case $confirmation in
+            [yY])
+                rm -rf "$DB_PATH"
+                echo -e "\e[41m Database '$DB_NAME' has been deleted successfully! \e[0m"
+                ;;
+            [nN])
+                echo "Operation cancelled. Returning to Main Menu..."
+                ;;
+            *)
+                echo "Invalid input! Returning to Main Menu..."
+                ;;
+        esac
+    else
+        echo -e "\e[41m Database '$DB_NAME' does not exist! \e[0m"
+    fi
+}
+
+
 welcomeScreen
+
 
